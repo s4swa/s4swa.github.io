713775c71a15d37014290b4b4887649eb0a31a0d
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-21 17:58:44 +0100

    Create handler.js

---
+++ assets/js/handler.js
@@ -0,0 +1,11 @@
+$(function() { 
+  var social = $(".page-wrapper");
+	
+  $(".nani").click(function() {
+    social.html(window.dnani.dhtml);	  
+  });
+  $(".").click(function() {
+    social.html(window.committee.dhtml);
+	  
+  });
+});
