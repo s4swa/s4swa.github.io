76ee069ef1cb17ca712342c44203a9f7b6f0f087
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-22 15:22:34 +0100

    Update handler.js

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -1,11 +1,1 @@
-$(function() { 
-  var social = $(".page-wrapper");
-	
-  $(".nani").click(function() {
-    social.html(window.dnani.dhtml);	  
-  });
-  $(".").click(function() {
-    social.html(window.committee.dhtml);
-	  
-  });
-});
+
